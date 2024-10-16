e41fc099e129f9618913bfeab2caaf02d9a83286
Barbara <barbarabarbabela@gmail.com>
2024-10-16 15:26:08 -0300

    feat: create the blip connection method

--- src/utils/blip-connection.ts
+++ src/utils/blip-connection.ts
@@ -1,8 +1,28 @@
 import { ClientBuilder } from "blip-sdk/dist/blip-sdk";
 import WebSocketTransport from "lime-transport-websocket";
 
-export const client = new ClientBuilder()
-  .withIdentifier(process.env.IDENTIFIER)
-  .withAccessKey(process.env.ACCESS_KEY)
-  .withTransportFactory(() => new WebSocketTransport())
-  .build();
+export async function blipConnection(identifier, accessKey) {
+  const client = new ClientBuilder()
+    .withIdentifier(identifier)
+    .withAccessKey(accessKey)
+    .withTransportFactory(() => new WebSocketTransport())
+    .build();
+
+  try {
+    const session = await client.connect();
+    console.log("Application started. Press Ctrl + C to stop.");
+
+    return session;
+  } catch (err) {
+    console.error("Connection failed:", err);
+    client
+      .close()
+      .then(function () {
+        /* Disconnection success */
+        console.log("Disconnected");
+      })
+      .catch(function (err) {
+        /* Disconnection failed */
+      });
+  }
+}
