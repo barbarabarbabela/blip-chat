79d2e7340ce3c3e3712554723512dc97bf28278e
Barbara <barbarabarbabela@gmail.com>
2024-10-16 18:05:10 -0300

    refactor: remove body from zod schema

--- src/schemas/api-key.schema.ts
+++ src/schemas/api-key.schema.ts
@@ -1,10 +1,8 @@
 import { z } from "zod";
 
 export const createApiKeySchema = z.object({
-  body: z.object({
-    identifier: z.string(),
-    accessKey: z.string(),
-  }),
+  identifier: z.string(),
+  accessKey: z.string(),
 });
 
 export type CreateApiKeySchema = z.infer<typeof createApiKeySchema>;
