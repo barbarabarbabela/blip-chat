39d86d209c08f257b339645ba04007f5b675b6c1
Barbara <barbarabarbabela@gmail.com>
2024-10-20 13:14:56 -0300

    refactor: remove utils functions

--- src/schemas/api-key.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const createApiKeySchema = z.object({
-  identifier: z.string(),
-  accessKey: z.string(),
-});
-
-export type CreateApiKeySchema = z.infer<typeof createApiKeySchema>;
