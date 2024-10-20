afe855db62cd7fb0a8b7185a51791ebff54723de
Barbara <barbarabarbabela@gmail.com>
2024-10-20 13:16:26 -0300

    reafactor: create auth controller

---
+++ src/modules/auth/controller/auth.controller.ts
@@ -0,0 +1,19 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import authSchemaValidator from "../schemas/auth.schema.validator";
+import authService from "../service/auth.service";
+
+const login = async (request: FastifyRequest, reply: FastifyReply) => {
+  const parsedBody = authSchemaValidator.apiKey(request.body);
+
+  const { apiKey } = parsedBody;
+
+  const token = reply.server.jwt.sign({ apiKey });
+
+  await authService.login(apiKey);
+
+  reply.status(200).send({ message: "Login realizado com sucesso!" });
+};
+
+export default {
+  login,
+};
