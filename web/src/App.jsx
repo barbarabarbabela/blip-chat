a95594cb2d6d34ffb1b91484fdebd90e9673d00d
Barbara <barbarabarbabela@gmail.com>
2024-10-20 20:00:05 -0300

    feat: create router

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,11 +1,19 @@
 
 import './App.css'
+import HomePage from './components/home-page'
+import LoginPage from './components/login-page'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      Hello World
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
     </>
   )
 }
