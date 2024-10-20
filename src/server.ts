a926becb0e4ec77e856eab37bf6b17524f4abce7
Barbara <barbarabarbabela@gmail.com>
2024-10-20 13:18:02 -0300

    refactor: remove fastify cookies for now

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import fastify from "fastify";
 import fastifyCors from "@fastify/cors";
 import { routes } from "./routes/routes";
-import fastifyCookie from "@fastify/cookie";
 
 const app = fastify();
 
 app.register(fastifyCors, {
   origin: "*",
 });
-
-app.register(fastifyCookie);
 
 app.register(routes);
 
