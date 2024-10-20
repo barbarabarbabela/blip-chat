f633fe2081979e0c6d169e2f2f3860c2637d980f
Barbara <barbarabarbabela@gmail.com>
2024-10-20 13:15:41 -0300

    refactor: create route module and isolate the logic to controllers and services

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,42 +1,10 @@
 import { FastifyInstance } from "fastify";
-import { createApiKeySchema } from "../schemas/api-key.schema";
-import { blipConnection } from "../utils/blip-connection";
-import jwt from "jsonwebtoken";
-import { authMiddleware } from "../utils/auth-middleware";
-import { env } from "../env";
-
-const JWT_SECRET = env.JWT_SECRET;
+import authController from "../modules/auth/controller/auth.controller";
+import contactsController from "../modules/contacts/controller/contacts.controller";
 
 export async function routes(fastify: FastifyInstance) {
-  fastify.post("/login", async (request, reply) => {
-    const { body } = createApiKeySchema.parse(request);
-
-    try {
-      await blipConnection(body.identifier, body.accessKey);
-
-      const token = jwt.sign(
-        {
-          identifier: body.identifier,
-          accessKey: body.accessKey,
-        },
-        JWT_SECRET,
-        { expiresIn: "1h" }
-      );
-
-      reply
-        .setCookie("authToken", token, {
-          httpOnly: true,
-          maxAge: 3600,
-        })
-        .redirect("/");
-    } catch (err) {
-      return reply.status(401).send({
-        message: "Autenticação falhou no Blip. Verifique suas credenciais.",
-      });
-    }
-  });
-
-  fastify.get("/", { preHandler: authMiddleware }, async (request, reply) => {
-    return reply.send({ message: "Acesso permitido à rota!" });
-  });
+  fastify.post("/login", authController.login);
+  fastify.get("/", contactsController.getContacts);
+  fastify.get("/messages", contactsController.getMessages);
+  fastify.get("/contato/:contactId", contactsController.getContactMessages);
 }
