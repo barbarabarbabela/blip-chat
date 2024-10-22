6da2ed7ceba68d086026b9b12348c41cc7639c2f
Barbara <barbarabarbabela@gmail.com>
2024-10-22 16:02:01 -0300

    feat: add pre handler to protected routes

--- api/src/routes/routes.ts
+++ api/src/routes/routes.ts
@@ -1,10 +1,23 @@
 import { FastifyInstance } from "fastify";
 import authController from "../modules/auth/controller/auth.controller";
 import contactsController from "../modules/contacts/controller/contacts.controller";
+import verifyToken from "../modules/auth/shared/auth.middleware";
 
 export async function routes(fastify: FastifyInstance) {
   fastify.post("/login", authController.login);
-  fastify.get("/", contactsController.getContacts);
-  fastify.get("/messages", contactsController.getMessages);
-  fastify.get("/contato/:id", contactsController.getContactMessages);
+  fastify.get(
+    "/",
+    { preHandler: [verifyToken] },
+    contactsController.getContacts
+  );
+  fastify.get(
+    "/messages",
+    { preHandler: [verifyToken] },
+    contactsController.getMessages
+  );
+  fastify.get(
+    "/contato/:id",
+    { preHandler: [verifyToken] },
+    contactsController.getContactMessages
+  );
 }
