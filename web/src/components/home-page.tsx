8a197a50d113df7f1ec0014919101bf2e3e90063
Barbara <barbarabarbabela@gmail.com>
2024-10-22 16:03:26 -0300

    feat: create home page

--- web/src/components/home-page.tsx
+++ web/src/components/home-page.tsx
@@ -1,26 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import { getContacts } from '../http/get-contacts';
-import ContactCard from './contact-card';
+import React from "react";
+import { useContactsQuery } from "../hooks/use-contacts";
+import ContactCard from "./contact-card";
+import { useContactsContext } from "../contexts/contacts-context";
+import DetailsModal from "./details-modal";
+import { Contact } from "../interfaces/contacts";
 
 function HomePage() {
-  // const [contacts, setContacts] = useState([]);
+  const { contacts } = useContactsContext();
+  const { data, isLoading } = useContactsQuery(contacts);
+  const [isDetailModalOpen, setIsDetailModalOpen] = React.useState(false);
+  const [selectedContact, setSelectedContact] = React.useState<Contact | null>(
+    null
+  );
 
-  // async function handleContacts() {
-  //    const response = await getContacts();
-  //   console.log(response)
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-  // }
+  if (!data || !data.contactList) {
+    return <div>No contacts found.</div>;
+  }
 
-  // useEffect(() => {
-  //   handleContacts();
-  // }, []);
+  const handleDetailModal = () => {
+    setIsDetailModalOpen(!isDetailModalOpen);
+
+  };
+
+  const handleContactClick = (contact: Contact) => {
+    setSelectedContact(contact);
+    setIsDetailModalOpen(true);
+  };
+
+  console.log(selectedContact)
 
   return (
     <div>
-      <h1 className='text-3xl p-10 font-bold'>Contatos registrados no bot</h1>
-      <ContactCard />
+      <h1 className="text-3xl p-10 font-bold ">Contatos registrados no bot</h1>
+      <div className="flex flex-wrap">
+        {data.contactList.map((contact) => (
+          <ContactCard
+            key={contact.identity}
+            name={contact.name}
+            onClickDetails={() => handleContactClick(contact)}
+          />
+        ))}
+      </div>
+
+      {isDetailModalOpen && selectedContact && (
+        <DetailsModal
+          isOpen={isDetailModalOpen}
+          title={"Detalhes do contato"}
+          onClose={handleDetailModal}
+          contact={selectedContact} 
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
