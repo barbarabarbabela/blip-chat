4ee43f4660a98af12842014dfac71db86eedea59
Barbara <barbarabarbabela@gmail.com>
2024-10-22 16:02:19 -0300

    feat: add query client provider to app component

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,23 +1,28 @@
-
-import './App.css'
-import HistoryPage from './components/history-page';
-import HomePage from './components/home-page'
-import LoginPage from './components/login-page'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import HistoryPage from "./components/history-page";
+import HomePage from "./components/home-page";
+import LoginPage from "./components/login-page";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ContactsProvider } from "./contexts/contacts-context";
 
 function App() {
-
+  const queryClient = new QueryClient();
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/contato" element={<HistoryPage />} />
-      </Routes>
-    </Router>
+      <ContactsProvider>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/contato" element={<HistoryPage />} />
+            </Routes>
+          </Router>
+        </QueryClientProvider>
+      </ContactsProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
