3621321c9bd49b16de8bb1c52ba418cc0dfe4d20
Barbara <barbarabarbabela@gmail.com>
2024-10-22 16:00:27 -0300

    feat: add jwt to the login route

--- api/src/modules/auth/controller/auth.controller.ts
+++ api/src/modules/auth/controller/auth.controller.ts
@@ -1,15 +1,29 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import authSchemaValidator from "../schemas/auth.schema.validator";
 import authService from "../service/auth.service";
+import jwt from "jsonwebtoken";
+
+const SECRET_KEY = process.env.JWT_SECRET || "your-secret-key";
 
 const login = async (request: FastifyRequest, reply: FastifyReply) => {
   const parsedBody = authSchemaValidator.apiKey(request.body);
-
   const { apiKey } = parsedBody;
 
-  await authService.login(apiKey);
+  try {
+    await authService.login(apiKey);
+    const token = jwt.sign({ apiKey }, SECRET_KEY, {
+      expiresIn: "1h",
+    });
 
-  reply.status(200).send({ message: "Login realizado com sucesso!" });
+    reply
+      .header("Authorization", `Bearer ${token}`)
+      .status(200)
+      .send({ token });
+  } catch (error) {
+    reply
+      .status(401)
+      .send({ message: "Falha no login. Verifique sua chave API." });
+  }
 };
 
 export default {
